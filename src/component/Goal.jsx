6d965d2be8558e5d5d179cd74791ed5e222f95b1
showone7 <li.andr1771@gmail.com>
2022-04-01 14:31:50 +0300

    mvp stage

--- src/component/Goal.jsx
+++ src/component/Goal.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { useDispatch } from "react-redux";
 import { delGoal } from "../reducers/goalsReducer";
 import { updateGoal } from "../reducers/goalsReducer";
+import { Modal } from "react-bootstrap";
 
 const Goal = (props) => {
   const dispatch = useDispatch();
+  const [visible, setVisible] = useState(false);
 
   const handleAchieved = () => {
+    setVisible(false);
     dispatch(updateGoal({ ...props.goal, achieved: true }));
   };
 
@@ -36,9 +39,27 @@ const Goal = (props) => {
               isValid
               type="switch"
               id="custom-switch"
-              onChange={handleAchieved}
+              onChange={() => setVisible(true)}
             />
           </Form>
+          <Modal
+            show={visible}
+            onHide={() => setVisible(false)}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Поздравляем!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Ты достиг своей цели. Продолжай в том же духе.
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="success" onClick={handleAchieved}>
+                Ура!
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       )}
       <div className="col-12 col-sm-1 d-grid gap-2">
--- src/component/Pages/Goals.jsx
+++ src/component/Pages/Goals.jsx
@@ -1,37 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useGoals } from "../../hooks/useGoals";
 import GoalForm from "../GoalForm";
 import GoalsFilter from "../GoalsFilter";
 import GoalsList from "../GoalsList";
-import MyLoader from "../UI/loader/MyLoader";
 
 function Goals() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [isGoalsLoading, setIsGoalsLoading] = useState(false);
 
   const newGoals = useSelector((state) => {
     return state.filter((g) => g.achieved !== true);
   });
-  console.log(123, newGoals);
 
   const sortedAndSearchedGoals = useGoals(newGoals, filter.sort, filter.query);
 
   return (
     <div>
-      {isGoalsLoading ? (
-        <MyLoader />
-      ) : (
-        <div>
-          <h1>Твои цели:</h1>
-          <div className="d-flex">
-            <GoalForm />
-          </div>
-          <GoalsFilter filter={filter} setFilter={setFilter} />
-
-          <GoalsList goals={sortedAndSearchedGoals} />
-        </div>
-      )}
+      <h1>Твои цели:</h1>
+      <div className="d-flex">
+        <GoalForm />
+      </div>
+      <GoalsFilter filter={filter} setFilter={setFilter} />
+      <GoalsList goals={sortedAndSearchedGoals} />
     </div>
   );
 }

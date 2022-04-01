a8414b9a6e8b064591029ce9c0da4e07412ba66f
showone7 <li.andr1771@gmail.com>
2022-04-01 15:06:01 +0300

    deploy

--- src/component/Pages/Goals.jsx
+++ src/component/Pages/Goals.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useGoals } from "../../hooks/useGoals";
 import GoalForm from "../GoalForm";
